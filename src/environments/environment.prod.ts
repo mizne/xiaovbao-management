e606a069c901b05a3f59cca9d52920207aa3e783
mizne <w20054319@126.com>
2017-11-15 16:47:19 +0800

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  SERVER_URL: `https://deal.xiaovbao.cn`,
+  // SERVER_URL: `https://deal.xiaovbao.cn`,
+  SERVER_URL: `https://salesProxy.xiaovbao.cn`,
   loggerLevel: 6,
   production: true,
   hmr: false
