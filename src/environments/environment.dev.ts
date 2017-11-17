3ca009903806a2ea9c25afb92fe106f8c42a5e33
mizne <w20054319@126.com>
2017-11-17 18:05:09 +0800

    remove environemnts

--- src/environments/environment.dev.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  SERVER_URL: `https://salesProxy.xiaovbao.cn`,
-  loggerLevel: 0,
-  production: false,
-  hmr: false,
-}
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  // SERVER_URL: `https://deal.xiaovbao.cn`,
-  SERVER_URL: `https://salesProxy.xiaovbao.cn`,
-  loggerLevel: 6,
-  production: true,
-  hmr: false,
-}
